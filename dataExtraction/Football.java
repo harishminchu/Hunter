20582905b5604653dd6d687b97ab9c181313cba1
Harish N <n.hari1988@gmail.com>
2013-07-18 16:21:56 +0530

    Optimized code
    This code has some sysout to understand the code better .

--- dataExtraction/Football.java
+++ dataExtraction/Football.java
@@ -8,25 +8,34 @@ import java.util.regex.Pattern;
 
 public class Football {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
 			Integer maxValue = 0;
 			Integer minValue = 0;
-			Integer temp = 0;
-			Integer mintemp = 0;
+			Integer goal = 0;
+			Integer mingoal = 0;
 			Integer count = 0;
 			Integer mincount = 0;
-
+			String tmaxName = "";
+			String tminName = "";
+			String teamName="";
 			Scanner scanner = new Scanner(new File(
 					"C:/Users/Toshiba/Downloads/football.dat"));
 
 			while (scanner.hasNext()) {
 
 				String string = scanner.nextLine();
+				
+
+				Matcher matcherName = Pattern.compile(".[A-Z][aA-zZ]{4,15}+")
+						.matcher(string);
+
+				while (matcherName.find()) {
+
+					teamName = matcherName.group();
+
+				}
 
 				Matcher matcher = Pattern.compile("\\d[0-9]\\s\\s-").matcher(
 						string);
@@ -36,7 +45,6 @@ public class Football {
 					maxValue = new Integer(matcher.group().replace("-", " ")
 							.trim());
 
-					System.out.println(maxValue);
 				}
 
 				Matcher matchermin = Pattern.compile("\\-\\s\\s[0-9]+")
@@ -46,36 +54,32 @@ public class Football {
 
 					minValue = new Integer(matchermin.group().replace("-", " ")
 							.trim());
-					System.out.println(minValue);
+
 				}
 
-				if (temp > (maxValue - minValue)) {
-					System.out.println(temp);
+				if (goal < (maxValue - minValue)) {
+					
+					goal = maxValue - minValue;
+					System.out.println(teamName);
+					System.out.println("max "+goal);
+					tmaxName = teamName;
+				} 
 
-				} else {
-					count = count + 1;
-					temp = maxValue - minValue;
+				if (mingoal > (maxValue - minValue)) {
+					mingoal = maxValue - minValue;
+					System.out.println("min "+mingoal);
+					System.out.println(teamName);
+					tminName = teamName;
 				}
-
-				if (mintemp < (maxValue - minValue)) {
-					System.out.println(mintemp);
-
-				} else {
-					mincount = mincount + 1;
-					mintemp = maxValue - minValue;
-				}
-				System.out.println("temp high" + mintemp);
 
 			}
 
-			System.out.println("team with max goal 1. Arsenal" + (count - 5));
-			System.out.println("team againts min goal  20. Leicester"
-					+ (mincount + 5));
+			System.out.println("team with max goal " + tmaxName  +" with goal "+ goal);
+			System.out.println("team with min goal" + tminName +" with goal "+mingoal);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-
 }
